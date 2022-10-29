61bdeae1bdc212b0bfbca0ad62d9b2c9b351a613
Aguinaldo Feliciano <aguinaldojunior@gec.inatel.br>
2022-10-29 20:42:13 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,34 @@
+export const state = [
+  {
+    quantity: 2,
+    supermarket: "Bergão",
+    id: "KLIE87ES-023IPDDPYG",
+    name: "Pizza",
+    isnecessary: true,
+    lastEdit: 1613990943287,
+  },
+  {
+    quantity: 1,
+    supermarket: "BH",
+    id: "KLIE1ST7-00IM8EM2Y6V7",
+    name: "Pão de Queijo",
+    isnecessary: false,
+    lastEdit: 1613897343287,
+  },
+  {
+    quantity: 5,
+    supermarket: "Alvorada",
+    id: "KLH80H0H-00TNHA9Z71KD",
+    name: "Maçã",
+    isnecessary: true,
+    lastEdit: 1614170943287,
+  },
+  {
+    quantity: 7,
+    supermarket: "Esquinão",
+    id: "KLH7ZQBT-026IOWX9I4N3",
+    name: "Coca-Café",
+    isnecessary: true,
+    lastEdit: 1614062943287,
+  },
+];
